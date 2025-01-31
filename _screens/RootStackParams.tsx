3e96a2dc53a02de86a5e25456e4cb7dbb3e1d4ed
AaronSmulian2004 <aaronsmulian14@gmail.com>
2025-01-31 12:43:12 +0200

    poe assignment set up

---
+++ _screens/RootStackParams.tsx
@@ -0,0 +1,16 @@
+
+
+export type Menu = {
+    Value: string;
+    DishName: string;
+    Description: string;
+    Course: string;
+    Price: number;
+};
+
+export type RootStackParamList = {
+    Home:  undefined; 
+    Addmenu: undefined;
+    FilterMenu: undefined; 
+};
+
